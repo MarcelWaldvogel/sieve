37353c1d5aa70be18e1b5651276b010b80d118a1
Artem Mavrin <amavrin@ucsd.edu>
2015-12-13 21:28:47 -0800

    Added divisors.c

---
+++ divisors.c
@@ -0,0 +1,62 @@
+/*******************************************************************************
+FILE:           divisors.c
+AUTHOR:         Artem Mavrin
+UPDATED:        2015-12-13
+DESCRIPTION:    Prints out the prime divisors of a given positive integer.
+                Prime divisors are computed using trial division with wheel
+                factorization.
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "wheel.h"
+
+const unsigned long basePrimes[] = {2, 3, 5, 7, 11, 13};
+const unsigned long numBasePrimes = 6;
+
+void print_divisors(unsigned long num) {
+    printf("Prime divisors of %lu:\n", num);
+    if (num > 1) {
+        unsigned long prime;
+        /* Divide by the base primes */
+        for (unsigned long index = 0; index < numBasePrimes; index++) {
+            prime = basePrimes[index];
+            while (num % prime == 0) {
+                printf("%lu\n", prime);
+                num /= prime;
+            }
+        }
+
+        /* Create the wheel for wheel factorization */
+        Wheel *wheel = newWheel(basePrimes, numBasePrimes);
+        nextp(wheel); /* Ignore 1 */
+        /* Divide by the prime candiates computed by the wheel */
+        while (num > 1) {
+            prime = nextp(wheel);
+            if (prime * prime > num) {
+                printf("%lu\n", num);
+                break;
+            }
+            while (num % prime == 0) {
+                printf("%lu\n", prime);
+                num /= prime;
+            }
+        }
+        /* Deallocate the memory associated with the wheel */
+        deleteWheel(&wheel);
+    }
+}
+
+int main(int argc, const char **argv) {
+    if (argc == 1) {
+        /* If there are no command line arguments, print usage message */
+        fprintf(stderr,
+            "Usage: Enter a positive integer: e.g.\n%s 12345\n", argv[0]);
+        /* Exit with failure */
+        return 1;
+    }
+
+    while (*(++argv)) {
+        print_divisors(strtoul(*argv, NULL, 10));
+    }
+}
